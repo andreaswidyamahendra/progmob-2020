68bf2a5e205f80fc01d4dbf3f5f07924178a5900
andreas widya mahendra <andreas.widya@si.ukdw.ac.id>
2020-09-30 00:10:18 +0700

    Tugas 2: Tracker Layout

---
+++ app/src/main/java/com/example/proteintracker/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.proteintracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView txtView = (TextView)findViewById(R.id.mainActivityTextView);
+        Button myBtn = (Button)findViewById(R.id.button1);
+        final EditText myEditText = (EditText)findViewById(R.id.editText1);
+        Button btnHelp = (Button)findViewById(R.id.btnHelp);
+        Button button3 = (Button)findViewById(R.id.button3);
+
+        txtView.setText(R.string.text_hello_world);
+        myBtn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Log.d("Klik", myEditText.getText().toString());
+                txtView.setText(myEditText.getText().toString());
+            }
+        }));
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HelpActivity.class);
+                Bundle b =  new Bundle();
+                b.putString("help_string",myEditText.getText().toString());
+                intent.putExtras(b);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TrackerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
